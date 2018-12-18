43f034e0dd4eb83de807d52ce216aa0549023c96
Korpel <Vozikisant@gmail.com>
2018-12-18 23:39:32 +0200

    asides

--- testing/TableViewController.swift
+++ testing/TableViewController.swift
@@ -56,17 +56,18 @@ class TableViewController: UITableViewController {
     }
     */
 
-    /*
+    
     // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Delete the row from the data source
+            someValues.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
+            print(someValues)
         } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+            
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
